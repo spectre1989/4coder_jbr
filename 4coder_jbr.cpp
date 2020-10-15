0c7771717b0846d57e6eb45c5fe066553bc37780
Joe Best-Rotheray <joebestrotheray@gmail.com>
2020-10-15 23:09:01 +0100

    removed some stuff which was copy pasta'd

--- 4coder_jbr.cpp
+++ 4coder_jbr.cpp
@@ -1,15 +1,4 @@
-/*
-4coder_default_bidings.cpp - Supplies the default bindings used for default 4coder behavior.
-*/
-
-// TOP
-
-#if !defined(FCODER_DEFAULT_BINDINGS_CPP)
-#define FCODER_DEFAULT_BINDINGS_CPP
-
 #include "4coder_default_include.cpp"
-
-// NOTE(allen): Users can declare their own managed IDs here.
 
 #if !defined(META_PASS)
 #include "generated/managed_id_metadata.cpp"
@@ -31,8 +20,3 @@ custom_layer_init(Application_Links* app) {
     setup_default_mapping(&framework_mapping, mapid_global, mapid_file, mapid_code);
 #endif
 }
-
-#endif //FCODER_DEFAULT_BINDINGS
-
-// BOTTOM
-
